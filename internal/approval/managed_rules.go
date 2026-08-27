18fbc60c7dc28dbed0c90cc80c01f87f81703e73
Tim Richardson <tim@growthpath.com.au>
2026-08-27 17:28:03 +1000

    fix(approval): reuse shared map clone helper

--- internal/approval/managed_rules.go
+++ internal/approval/managed_rules.go
@@ -471,17 +471,6 @@ func cloneManagedTrustRule(rule ManagedTrustRule) ManagedTrustRule {
 	return rule
 }
 
-func cloneStringMap(values map[string]string) map[string]string {
-	if values == nil {
-		return nil
-	}
-	cloned := make(map[string]string, len(values))
-	for key, value := range values {
-		cloned[key] = value
-	}
-	return cloned
-}
-
 func managedTrustRulesEqual(a, b ManagedTrustRule) bool {
 	return slices.Equal(a.RequestTypes, b.RequestTypes) &&
 		processMatchersEqual(a.Process, b.Process) &&
